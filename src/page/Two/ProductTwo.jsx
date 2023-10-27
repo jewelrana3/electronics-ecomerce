ef4ea3e8007c66c7798b73dd8ae8c7f53a7202a3
Jewel Rana <juyelrana7752@gmail.com>
2023-10-27 10:28:36 +0600

    add wishlist

--- src/page/Two/ProductTwo.jsx
+++ src/page/Two/ProductTwo.jsx
@@ -7,28 +7,28 @@ import { BiLink } from 'react-icons/bi';
 
 const ProductTwo = ({ product }) => {
 
-    const { image, title, price,_id,suk,category,tags,Share } = product;
+    const { image, title, price, _id, suk, category, tags, Share } = product;
     const [isHovered, setIsHovered] = useState(false);
 
     // add to cart
-    const addToCart = item =>{
-        const navItem = {classId:_id,image,title,price,suk,category,tags,Share}
-        fetch('http://localhost:5000/addCartPost',{
-            method:'POST',
-            headers:{
-                'content-type':'application/json'
+    const addToCart = item => {
+        const navItem = { classId: _id, image, title, price, suk, category, tags, Share }
+        fetch('http://localhost:5000/addCartPost', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
             },
-            body:JSON.stringify(navItem)
+            body: JSON.stringify(navItem)
         })
-        .then(res => res.json())
-        .then(data => {
-            if (data.insertedId) {
-                refetch();
-                
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    refetch();
+
+                }
+            })
     }
-  
+
 
 
 
@@ -39,27 +39,36 @@ const ProductTwo = ({ product }) => {
             onMouseLeave={() => setIsHovered(false)}
         >
             <div className="image-content">
-              
+
                 <img
                     src={image ? image : 'image1'}
                     alt="product image"
                     className="hovered-image active"
-                    
+
                 />
                 <ToastContainer />
-               
+
                 <div className={`overlay ${isHovered ? 'active' : ''}`}>
-                    <button onClick={() => addToCart(toast(`${title} add to cart`)) } className="add_to_cart w-full bg-black text-white">
+                    <button onClick={() => addToCart(toast(`${title} add to cart`))} className="add_to_cart w-full bg-black text-white">
                         Add Cart
                     </button>
                 </div>
                 <div className="leftDrawer text-2xl cursor-pointer">
-                    <p className=""><GiSelfLove /></p>
-                    <p className=""><AiOutlineEye /></p>
-                    <p className="">  <BiLink /></p>
+                    <div>
+                        <p className="icon"><GiSelfLove /></p>
+                        <span className="text">Add To Wishlist</span>
+                    </div>
+                    <div>
+                        <p className="icon"><AiOutlineEye /></p>
+                        <span className="text">View Details</span>
+                    </div>
+                    <div>
+                        <p className="icon"><BiLink /></p>
+                        <span className="text">Product Details</span>
+                    </div>
                 </div>
             </div>
-          
+
             <div className="product_title ">
                 <h3>{title}</h3>
                 <div>
